42f029003dc74ef2f6b74dd5690017bc1e6ceb81
yenox <f3lixxx@ya.ru>
2024-05-11 13:17:04 +0300

    modified SOLID HW1

--- SOLID_DRY/HW2/main.cpp
+++ SOLID_DRY/HW2/main.cpp
@@ -5,64 +5,75 @@
 
 class Printable {
 public:
-	virtual std::string printformat(std::ofstream& file) const = 0;
+    virtual std::string printformat() const = 0;
 };
 
-class HTML : public Printable
-{
+class HTML : public Printable {
 public:
-	HTML(std::string data) : data_(std::move(data)) {};
-	std::string printformat(std::ofstream& file) const override{
+    HTML(std::string data) : data_(std::move(data)) {}
+    std::string printformat() const override {
         file.open("C:\\Programming\\Design_Pattrens_CPP\\SOLID_DRY\\HW2\\HTML.txt");
-		file << data_;
-		return "html" + data_ + "html/";
-	}
+        if (file.is_open()) {
+            file << data_;
+            return "html" + data_ + "html/";
+        }
+        return "Failed to open HTML file.";
+    }
+
 private:
-	std::string data_;
+    std::string data_;
 };
 
-class TEXT : public Printable
-{
+class TEXT : public Printable {
 public:
-	TEXT(std::string data) : data_(std::move(data)) {};
-	std::string printformat(std::ofstream& file) const override {
+    TEXT(std::string data) : data_(std::move(data)) {}
+    std::string printformat() const override {
         file.open("C:\\Programming\\Design_Pattrens_CPP\\SOLID_DRY\\HW2\\TEXT.txt");
-		file << data_;
-		return "TEXT" + data_ + "TEXT/";
-	}
+        if (file.is_open()) {
+            file << data_;
+            return "TEXT" + data_ + "TEXT/";
+        }
+        return "Failed to open TEXT file.";
+    }
+
 private:
-	std::string data_;
+    std::string data_;
 };
 
-class JSONPRINT : public Printable
-{
+class JSON : public Printable {
 public:
-	JSONPRINT(std::string data) : data_(std::move(data)) {};
-	std::string printformat(std::ofstream& file) const override {
+    JSON(std::string data) : data_(std::move(data)) {}
+    std::string printformat() const override {
         file.open("C:\\Programming\\Design_Pattrens_CPP\\SOLID_DRY\\HW2\\JSON.txt");
-		file << data_;
-		return "JSON" + data_ + "JSON";
-	}
-private:
-	std::string data_;
-};
+        if (file.is_open()) {
+            file << data_;
+            return "JSON" + data_ + "JSON";
+        }
+        return "Failed to open JSON file.";
+    }
 
+private:
+    std::string data_;
+};
 
 int main() {
-	std::ofstream filename;
-	std::string str;
-    std::cout << "HTML: ";
-    std::getline(std::cin,str);
-    HTML filehtml(str);
-    filehtml.printformat(filename);
-    std::cout << "TEXT: ";
-    std::getline(std::cin,str);
-    TEXT text(str);
-    text.printformat(filename);
-    JSONPRINT jsonformat(str);
-    std::cout << "JSON: ";
-    std::getline(std::cin,str);
-    jsonformat.printformat(filename);
+    std::ofstream filename;
+    std::string str;
 
-	return 0;
+    std::cout << "HTML: ";
+    std::getline(std::cin, str);
+    HTML filehtml(str);
+    std::cout << filehtml.printformat(filename) << std::endl;
+
+    std::cout << "TEXT: ";
+    std::getline(std::cin, str);
+    TEXT text(str);
+    std::cout << text.printformat(filename) << std::endl;
+
+    std::cout << "JSON: ";
+    std::getline(std::cin, str);
+    JSON jsonformat(str);
+    std::cout << jsonformat.printformat(filename) << std::endl;
+
+    return 0;
 }
